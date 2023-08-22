4bd490550fc508c0b25ff2f1ac02e4c7da542570
shanto096 <shanto096@gmail.com>
2023-08-23 02:38:23 +0600

    final2

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const port = process.env.port || 5000;
 
 app.use(cors())
 
-const categories = require("./data/categories.json")
+const categories = require("./data/category.json")
+const news = require("./data/news.json")
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD")
-})
-app.get('/categories', (req, res) => {
-    console.log(categories);
+app.get('/category', (req, res) => {
     res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const newsId = news.find(n => n._id === id)
+    res.send(newsId)
+})
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+
 })
 
 app.listen(port, () => {
